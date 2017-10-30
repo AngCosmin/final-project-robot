010dd4ac4eca3e0f79ff038e7fa2437f58e24d26
Cosmin <cosminzorr@gmail.com>
2017-10-30 18:12:51 +0200

    Minor changes

--- car/connect_to_server.py
+++ car/connect_to_server.py
@@ -6,33 +6,10 @@ import sys
 import tty
 import termios
 
-def readchar():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    if ch == '0x03':
-        raise KeyboardInterrupt
-    return ch
-
-def readkey(getchar_fn=None):
-    getchar = getchar_fn or readchar
-    c1 = getchar()
-    if ord(c1) != 0x1b:
-        return c1
-    c2 = getchar()
-    if ord(c2) != 0x5b:
-        return c1
-    c3 = getchar()
-    return ord(c3) - 65  # 0=Up, 1=Down, 2=Right, 3=Left arrows
-
 def on_message(ws, message):
     try:
         messageJSON = json.loads(message)
-        print("Server said: " + messageJSON['status'])
+        print ("SERVER: " + messageJSON['status'] + " " + messageJSON['message'])
     except Exception as e:
         print (e)
 
@@ -43,14 +20,13 @@ def on_close(ws):
     print("### closed ###")
 
 def on_open(ws):
-    ws.send("Hello")
-
-    while True:
-        keyp = readkey()
-        if keyp == 'w':
-            ws.send('Forward')
-        elif keyp == 's':
-            ws.send('Backward')
+    ws.send("Connected")
+    # for i in range(0, 5):
+    #     age = input("Your age? ")
+    #     ws.send(str(age))
+    while (True):
+        command = raw_input("What do you want to do? ")
+        ws.send(command)
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
--- server/server.js
+++ server/server.js
@@ -1,13 +1,30 @@
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 3000 });
+var clients = {};
 
-wss.on('connection', function (ws) {
+wss.on('connection', function (ws, req) {
     console.log('New connection!');
+
+    // Generate client unique ID
+    var clientId = new Date().getUTCMilliseconds();
+    clients[clientId] = ws;
+
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({'status': 'success', 'message': 'We have a new connection! Welcome client ' + clientId}));
+        }
+    });
 
     ws.on('message', function (message) {
         console.log('received: %s', message);
+
+        ws.send(JSON.stringify({'status': 'success', 'message': 'Your message was received. You said ' + message}));
     });
 
-    ws.send(JSON.stringify({'status': 'success', 'message': 'something'}));
+    ws.on('close', function () {
+        console.log('Disconnected');
+
+        delete clients[clientId];
+    });
 });
