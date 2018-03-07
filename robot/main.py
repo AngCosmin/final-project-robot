e3f26c1ec61d74b5550cd8bdb6d6e7d30360f1be
Cosmin <cosminzorr@gmail.com>
2018-03-07 22:04:36 +0200

    Thread update

--- robot/main.py
+++ robot/main.py
@@ -3,6 +3,8 @@ import websocket
 import thread
 import json
 from time import sleep
+from threading import Thread
+from Queue import Queue
 from controllers.MotorsController import MotorsController
 from controllers.RelayController import RelayController
 from controllers.UltrasonicController import UltrasonicController
@@ -10,7 +12,7 @@ from controllers.UltrasonicController import UltrasonicController
 ws = None
 ultrasonicDistance = 0
 
-def threadCalculateUltrasonicDistance():
+def thread_calculate_ultrasonic_distance(thread_name):
     global ultrasonicDistance
 
     while True:
@@ -60,6 +62,8 @@ if __name__ == "__main__":
         server_port = config.get('Websocket', 'server_port')
 
         print 'Connecting to ' + server_ip + ':' + server_port + '...'
+
+        thread.start_new_thread(thread_calculate_ultrasonic_distance, ("Thread-1"))
 
         websocket.enableTrace(True)
         ws = websocket.WebSocketApp('ws://' + server_ip + ':' + server_port)
