bab0d1cabb59f2024b146b9dcce631275bd51c44
Cosmin <cosminzorr@gmail.com>
2018-03-19 21:43:03 +0200

    Minor changes

--- robot/ring.py
+++ robot/ring.py
@@ -23,6 +23,7 @@ if __name__ == '__main__':
     for i in range(0, strip.numPixels(), 1):
         strip.setPixelColor(i, Color(255, 255, 255))
 
+    time.sleep(1);
     # while True:
     #     now = datetime.datetime.now()
 
