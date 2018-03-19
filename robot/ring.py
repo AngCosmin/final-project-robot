3077ad646db1968e09d5685b4b7ffaf74e5e3b77
Cosmin <cosminzorr@gmail.com>
2018-03-19 23:29:22 +0200

    Minor changes

--- robot/ring.py
+++ robot/ring.py
@@ -5,34 +5,31 @@ import math
 from neopixel import *
 
 LED_COUNT = 16      # Number of LED pixels.
-LED_PIN = 12      # GPIO pin connected to the pixels (must support PWM!).
+LED_PIN = 13      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10       # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 10  # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 100  # Set to 0 for darkest and 255 for brightest
 # True to invert the signal (when using NPN transistor level shift)
 LED_INVERT = False
+LED_CHANNEL = 1 # set to '1' for GPIOs 13, 19, 41, 45, 53
 
 if __name__ == '__main__':
-    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
     strip.begin()
     strip.show()
 
-    strip.setPixelColor(15, Color(255, 0, 0))
-    strip.setPixelColor(14, Color(0, 255, 0))
-    strip.setPixelColor(13, Color(0, 0, 255))
+    while True:
+        for i in range(0, strip.numPixels(), 1):
+            strip.setPixelColor(i, Color(0, 0, 0))
 
-    # while True:
-    #     for i in range(0, strip.numPixels(), 1):
-    #         strip.setPixelColor(i, Color(0, 0, 0))
+        time.sleep(1)
 
-    #     time.sleep(1)
-
-    #     for i in range(0, strip.numPixels(), 1):
-    #         strip.setPixelColor(i, Color(255, 0, 0))
+        for i in range(0, strip.numPixels(), 1):
+            strip.setPixelColor(i, Color(255, 0, 0))
         
-    #     time.sleep(1)
+        time.sleep(1)
     # while True:
-    #     now = datetime.datetime.now()
+    #     now = datetime.datetime.now()1
 
     #     # Low light during 19-8 o'clock
     #     if(8 < now.hour < 19):
