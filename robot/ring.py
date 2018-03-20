c367b85da7f9464d05a1a290b04533ebdac198a8
Cosmin <cosminzorr@gmail.com>
2018-03-20 16:10:03 +0200

    RGB Lights changes

--- robot/ring.py
+++ robot/ring.py
@@ -47,6 +47,17 @@ def pulse(color):
             strip.setBrightness(i)    
             strip.show()
 
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
 def rainbow_cycle(wait_ms=20, iterations=5):
     for j in range(256*iterations):
         for i in range(strip.numPixels()):
