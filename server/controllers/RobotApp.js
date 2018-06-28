46d55c634498484aa96948562d4ebb2c42a437b4
Cosmin <cosminzorr@gmail.com>
2018-06-28 09:19:50 +0300

    Minor fixes

--- server/controllers/RobotApp.js
+++ server/controllers/RobotApp.js
@@ -8,26 +8,20 @@ class RobotApp {
 
     checkConnection() {
         let timePassed = 0;
+        let now = parseInt(Date.now() / 1000)
 
         if (this.pongReceivedTimestamp != null) {
-            // Calcaulate 'now' because there is a difference between python fuction for 
-            // timestamp and javascript function
-            let now = parseInt(Date.now() / 1000)
             timePassed = now - this.pongReceivedTimestamp;
         }
 
         if (timePassed > 2000) {
             // Didn't received a response for 2 seconds => mobile app disconnected
 
-            this.socket = null;
-            return false;
+            // this.socket = null;
+            // return false;
         }
         else if (this.socket != null && this.socket.readyState == WebSocket.OPEN) {
             // Mobile app is still connected
-
-            // Calcaulate 'now' because there is a difference between python fuction for 
-            // timestamp and javascript function
-            let now = parseInt(Date.now() / 1000)
 
             this.socket.send(JSON.stringify({ 'event': 'ping', 'timestamp': now }));
             return true;
